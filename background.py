37ff9015c3ac81caddadac1d5d844238d823edff
huangkg8 <39199470+huangkg8@users.noreply.github.com>
2018-05-23 15:36:26 +0800

    Add files via upload

---
+++ background.py
@@ -0,0 +1,19 @@
+import pygame as pg
+from pygame.sprite import Group
+from pygame.sprite import Sprite
+
+class Tree(Sprite):
+	pass
+
+class Springboard(Sprite):
+	pass
+
+class Trench(Sprite):
+	pass
+
+#others,如地雷，各种食品或装备
+
+class Background():
+	def __init__(self):
+		self.bg_factors=Group()
+		#向bg_factors里面添加元素，也可以分多个编组
